34239f40a9ef38134aa93ea042a041aee8248abd
Devin <devinrobichaux@gmail.com>
2018-01-20 17:17:23 -0600

    [schedule] interest type render from database on gather

--- Components/Event.js
+++ Components/Event.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import openMap from 'react-native-open-maps';
 // import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Button, View, StyleSheet, Image, Row } from 'react-native';
+import { Text, Button, View, StyleSheet, Image, Row, TouchableHighlight, Animated, Easing, Dimensions, Platform } from 'react-native';
 import { MapView } from 'expo';
 // https://github.com/react-community/react-native-maps for more information on how this library works
 import logo from '../img/logo.png';
-import SortableListView from 'react-native-sortable-listview'
+import SortableList from 'react-native-sortable-list'
+
+const window = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +16,34 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+    color: '#222222',
+  },
+  title: {
+    fontSize: 20,
+    paddingVertical: 20,
+    color: '#999999',
+  },
+
+  list: {
+    flex: 1,
+  },
+
   contentContainer: {
     width: window.width,
-    paddingHorizontal: 30,
+
+    ...Platform.select({
+      ios: {
+        paddingHorizontal: 30,
+      },
+
+      android: {
+        paddingHorizontal: 0,
+      }
+    })
   },
+
   row: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -28,17 +54,88 @@ const styles = StyleSheet.create({
     marginTop: 7,
     marginBottom: 12,
     borderRadius: 4,
-    width: window.width - 30 * 2,
-    shadowColor: 'rgba(0,0,0,0.2)',
-    shadowOpacity: 1,
-    shadowOffset: {height: 2, width: 2},
-    shadowRadius: 2,
-  },
-  text: {
-    fontSize: 24,
-    color: '#222222',
+
+
+    ...Platform.select({
+      ios: {
+        width: window.width - 30 * 2,
+        shadowColor: 'rgba(0,0,0,0.2)',
+        shadowOpacity: 1,
+        shadowOffset: {height: 2, width: 2},
+        shadowRadius: 2,
+      },
+
+      android: {
+        width: window.width - 30 * 2,
+        elevation: 0,
+        marginHorizontal: 30,
+      },
+    })
   },
 });
+
+const data = {
+  0: {
+    image: 'https://placekitten.com/200/240',
+    text: 'Chloe',
+  },
+  1: {
+    image: 'https://placekitten.com/200/201',
+    text: 'Jasper',
+  },
+  2: {
+    image: 'https://placekitten.com/200/202',
+    text: 'Pepper',
+  },
+  3: {
+    image: 'https://placekitten.com/200/203',
+    text: 'Oscar',
+  },
+  4: {
+    image: 'https://placekitten.com/200/204',
+    text: 'Dusty',
+  },
+  5: {
+    image: 'https://placekitten.com/200/205',
+    text: 'Spooky',
+  },
+  6: {
+    image: 'https://placekitten.com/200/210',
+    text: 'Kiki',
+  },
+  7: {
+    image: 'https://placekitten.com/200/215',
+    text: 'Smokey',
+  },
+  8: {
+    image: 'https://placekitten.com/200/220',
+    text: 'Gizmo',
+  },
+  9: {
+    image: 'https://placekitten.com/220/239',
+    text: 'Kitty',
+  },
+};
+class Basic extends React.Component {
+  render() {
+    console.log('hey from row')
+    console.log(this.props, 'this is props')
+    console.log(data, 'dayInfo');
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>React Native Sortable List</Text>
+        <SortableList
+          style={styles.list}
+          contentContainerStyle={styles.contentContainer}
+          data={data}
+          renderRow={this._renderRow} />
+      </View>
+    )
+  }
+  _renderRow = ({data, active}) => {
+    return <Event data={data} active={active} />
+  }
+}
 
 
 
@@ -49,16 +146,65 @@ export default class Event extends React.Component {
     this.state = {
     };
     this.openNewMap = this.openNewMap.bind(this);
+    this._active = new Animated.Value(0);
+  
+
+    this._style = {
+      ...Platform.select({
+        ios: {
+          transform: [{
+            scale: this._active.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 1.1],
+            }),
+          }],
+          shadowRadius: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [2, 10],
+          }),
+        },
+
+        android: {
+          transform: [{
+            scale: this._active.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 1.07],
+            }),
+          }],
+          elevation: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [2, 6],
+          }),
+        },
+      })
+    };
   }
   
   openNewMap() {
     // Get the coordinates out of the event's info
     openMap({ latutude: 40.7128, longitude: -74.0060 });
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.active !== nextProps.active) {
+      Animated.timing(this._active, {
+        duration: 300,
+        easing: Easing.bounce,
+        toValue: Number(nextProps.active),
+      }).start();
+    }
+  }
+  
+
   
   render() {
+    console.log('this is props', this.props);
     console.log('dayinfo', this.props.dayInfo);
     const events = Object.keys(this.props.dayInfo);
+    const { dayInfo } = this.props;
+    let active = 0;
+    console.log('this is data', dayInfo);
+    console.log('this is active', active);
     const eventNames = events.map((event, i) => (<Text key={`day${i}`} >{this.props.dayInfo[event].name}</Text>));
     const eventCoordinates = events.map((event) => { 
       return { title: this.props.dayInfo[event].name, coordinates: this.props.dayInfo[event].location };
@@ -82,69 +228,18 @@ export default class Event extends React.Component {
           </MapView>
         </View>
         {eventNames}
-        <SortableListView
-        style={{ flex: 1 }}
-        data={eventNames}
-        onRowMoved={e => {
-          eventNames.splice(e.to, 0, eventNames.splice(e.from, 1)[0])
-          this.forceUpdate()
-        }}
-        renderRow={row => <RowComponent data={row} />}
-      />
+        <Animated.View style={[
+          styles.row,
+          this._style,
+        ]}>
+          {/* <Image source={{uri: data.image}} style={styles.image} /> */}
+          <Text style={styles.text}>{dayInfo.name}</Text>
+        </Animated.View>
         <Button
           title="Go to Dashboard"
           onPress={() => this.props.navigation.navigate('Dashboard')}
         />
       </View>
-    );
-  }
-  _renderRow = ({eventNames, active}) => {
-    return <Row data={eventNames} active={active} />
-  }
-}
-
-class Row extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this._active = new Animated.Value(0);
-
-    this._style = {
-          transform: [{
-            scale: this._active.interpolate({
-              inputRange: [0, 1],
-              outputRange: [1, 1.1],
-            })
-          }],
-          shadowRadius: this._active.interpolate({
-            inputRange: [0, 1],
-            outputRange: [2, 10],
-          }),
-        }
-      }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.active !== nextProps.active) {
-      Animated.timing(this._active, {
-        duration: 300,
-        easing: Easing.bounce,
-        toValue: Number(nextProps.active),
-      }).start();
-    }
-  }
-
-  render() {
-   const {eventNames, active} = this.props;
-
-    return (
-      <Animated.View style={[
-        styles.row,
-        this._style,
-      ]}>
-        {/* <Image source={{uri: data.image}} style={styles.image} /> */}
-    <Text style={styles.text}>{eventNames}</Text>
-      </Animated.View>
     );
   }
 }
--- Components/Gather-Interests.js
+++ Components/Gather-Interests.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { Button } from 'react-native-elements'
+import axios from 'axios';
 
 
 const styles = StyleSheet.create({
@@ -26,16 +27,30 @@ const styles = StyleSheet.create({
 export default class GatherInterests extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+  this.state = {
+    selected: [],
+    types: []
+  }
+    // this.handleClick = this.handleClick.bind(this);
     this.handleNext = this.handleNext.bind(this);
-  }
+    // this.componentWillMount = this.componentWillMount.bind(this);
+  };
   componentWillMount() {
-    // In practice, we'll pull out all of the items and map over them,
-    // creating the buttons that way. I've hard-coded them here while
-    // we're waiting for database connection though
+
+    var self = this;
+
+    axios.get('http://18.218.102.64/types')
+    .then(function (response) {
+      console.log(response);
+      self.setState({ types: response.data.map(type => type.name )});
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   }
-  handleClick() {
-    console.log('something was clicked');
+  handleClick(e) {
+    console.log(e.target, 'just a string');
   }
   handleNext() {
     // Send information to database
@@ -43,7 +58,15 @@ export default class GatherInterests extends React.Component {
   }
   render() {
     const interests = [];
-    for (let i = 0; i < 50; i += 1) {
+    
+
+
+    for (let i = 0; i < this.state.types.length; i += 1) {
+      let name = this.state.types[i];
+      name = `${name.replace(/_{1,}/g,' ').replace(/(\s{1,}|\b)(\w)/g, function(m, space, letter) {
+        return space + letter.toUpperCase();
+      })}s`
+
       interests.push(
         <Button
           large
@@ -51,9 +74,9 @@ export default class GatherInterests extends React.Component {
           buttonStyle={{backgroundColor: '#0e416d', width: 500, marginVertical: 5 }}
           // style={styles.button}
           icon={{name: 'envira', type: 'font-awesome'}}
-          onPress={this.handleClick}
+          onPress={this.handleClick.bind(this)}
           key={i}
-          title='interests'
+          title={name}
         />
           // ,{/* <Text>interests {i}</Text> */}
         );
