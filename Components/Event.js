50d2e9bc912144c1e468af701b2f4cd0f0dcdc9b
Devin <devinrobichaux@gmail.com>
2018-01-22 18:40:57 -0600

    [list] renders two items from agenda but not working

--- Components/Event.js
+++ Components/Event.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import openMap from 'react-native-open-maps';
 // import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Button, View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { Text, Button, View, StyleSheet, Image, ImageBackground, ScrollView } from 'react-native';
 import { MapView } from 'expo';
 // https://github.com/react-community/react-native-maps for more information on how this library works
 import logo from '../img/logo.png';
 import { List, ListItem, Header } from 'react-native-elements';
+import SortableGrid from 'react-native-sortable-grid';
 
 const styles = StyleSheet.create({
   container: {
@@ -21,7 +22,7 @@ export default class Event extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     };
     this.openNewMap = this.openNewMap.bind(this);
   }
@@ -40,8 +41,15 @@ export default class Event extends React.Component {
 
   render() {
     const events = Object.keys(this.props.dayInfo);
-    const eventNames = events.map((event, i) => (<ListItem key={`day${i}`} title={this.props.dayInfo[event].name}></ListItem>));
-    const eventCoordinates = events.map((event) => { 
+    const itemOrder = this.props.dayInfo;
+    
+    console.log('this is events', events);
+    const eventNames = events.map((event, i) => (
+      <View style={{ paddingBottom: 1 }} key={`event${i}`} ref={'event_num' + i}>
+        <Text>{this.props.dayInfo[event].name}</Text>
+      </View>
+    ));
+    const eventCoordinates = events.map((event) => {
       return { title: this.props.dayInfo[event].name, coordinates: this.props.dayInfo[event].location };
     });
     const eventMarkers = eventCoordinates.map(coor => (<MapView.Marker coordinate={coor.coordinates} title={coor.title} key={coor.title} />));
@@ -53,9 +61,9 @@ export default class Event extends React.Component {
     };
     return (
       <View style={styles.container}>
-      <Header backgroundColor="#0e416d"
+        <Header backgroundColor="#0e416d"
           centerComponent={{ text: 'wander', style: { color: 'white', fontSize: 40, fontWeight: 'bold' } }}
-      
+
         />
         {/* <Image source={logo} style={{ width: 100, height: 100 }} /> */}
         <View style={{ width: 400, height: 200 }}>
@@ -66,9 +74,21 @@ export default class Event extends React.Component {
             {eventMarkers}
           </MapView>
         </View>
-          <List>
-        {eventNames}
-          </List>
+
+        <SortableGrid
+          blockTransitionDuration={400}
+          activeBlockCenteringDuration={200}
+          itemsPerRow={1}
+          dragActivationTreshold={200}
+          ref={ 'SortableGrid' }
+          onDragRelease={(itemOrder) => console.log("Drag was released, the blocks are in the following order: ", itemOrder)}
+          onDragStart={() => console.log("Some block is being dragged now!")} >
+
+        
+          {eventNames}
+
+        </SortableGrid>
+
         <Button
           title="Save your Trip Recommendations"
           onPress={this.saveTrip}
